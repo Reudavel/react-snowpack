bd7b688278f3d5e670ceed5f85fb92e894bfa124
Raul <reudavel@gmail.com>
2021-09-12 02:38:08 -0500

    Add react snowpack basics

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+/* Add JavaScript code here! */
+/*console.log('Hello World! You did it! Welcome to Snowpack :D');*/
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+ReactDOM.render(
+   <React.StrictMode>
+     <App />
+   </React.StrictMode>,
+   document.getElementById('root'),
+ );
